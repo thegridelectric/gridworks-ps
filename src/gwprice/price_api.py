c90bf0e491bfa1e1623f06159510c394cade217b
thdfw <th.defauw@gmail.com>
2025-11-19 17:49:01 +0100

    Stetson prices for next weekend

--- src/gwprice/price_api.py
+++ src/gwprice/price_api.py
@@ -130,36 +130,39 @@ class PriceApi():
             if (
                 next_hour >= pendulum.datetime(2025, 11, 14, 12, tz=self.timezone_str) and
                 next_hour < pendulum.datetime(2025, 11, 17, 12, tz=self.timezone_str)
+            ) or (
+                next_hour >= pendulum.datetime(2025, 11, 21, 12, tz=self.timezone_str) and
+                next_hour < pendulum.datetime(2025, 11, 24, 12, tz=self.timezone_str)
             ):
                 hack = True
                 df = pd.read_csv("data/trial_stetson_prices/stetson_sequence1.csv")
-                df_today = df[df['day'] == next_hour.day]
-                df_tomorrow = df[df['day'] == next_hour.day + 1]
+                df_today = df[df['day'].isin([next_hour.day, next_hour.day-7])]
+                df_tomorrow = df[df['day'].isin([next_hour.day+1, next_hour.day-7+1])]
                 if not df_today.empty:
                     prices_today_hack = list(df_today['lmp'])
                 if not df_tomorrow.empty:
                     prices_tomorrow_hack = list(df_tomorrow['lmp'])
 
                 # Friday
-                if next_hour.day == 14:
+                if next_hour.day in [14, 21]:
                     if len(prices_today_hack) == 12 and prices_today:
                         prices_today = prices_today[:12] + prices_today_hack
                     if len(prices_tomorrow_hack) == 24:
                         prices_tomorrow = prices_tomorrow_hack
                 # Satuday
-                if next_hour.day == 15:
+                if next_hour.day in [15, 22]:
                     if len(prices_today_hack) == 24:
                         prices_today = prices_today_hack
                     if len(prices_tomorrow_hack) == 24:
                         prices_tomorrow = prices_tomorrow_hack
                 # Sunday
-                if next_hour.day == 16:
+                if next_hour.day in [16, 23]:
                     if len(prices_today_hack) == 24:
                         prices_today = prices_today_hack
                     if len(prices_tomorrow_hack) == 12 and prices_tomorrow:
                         prices_tomorrow = prices_tomorrow_hack + prices_tomorrow[12:]
                 # Monday
-                if next_hour.day == 17:
+                if next_hour.day in [17, 24]:
                     if len(prices_today_hack) == 12 and prices_today:
                         prices_today = prices_today_hack + prices_today[12:]
 
