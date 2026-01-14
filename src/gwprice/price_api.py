0ab25174c8a3941f02e19193537bf91e3cb89023
thdfw <th.defauw@gmail.com>
2026-01-14 18:09:39 +0100

    Price forecasts now extended to 96 hours

--- src/gwprice/price_api.py
+++ src/gwprice/price_api.py
@@ -178,7 +178,8 @@ class PriceApi():
                 lmp_prices = prices_today[next_hour.hour:] + prices_tomorrow + prices_tomorrow[:next_hour.hour]
 
             # Extend to 96 hours with just the distribution prices
-            lmp_prices = lmp_prices + [lmp_prices[-1]] * 48
+            lmp_prices = lmp_prices + [0] * 48
+            lmp_prices = lmp_prices[:96]
             unix_times = [next_hour.in_timezone(self.timezone_str).timestamp() + i*3600 for i in range(96)]
             dist_prices = [self.get_dist_price(x.hour, x.weekday()) for x in [pendulum.from_timestamp(x, tz=self.timezone_str) for x in unix_times]]
             energy_prices = [x+y for x, y in zip(lmp_prices, dist_prices)]
@@ -186,7 +187,7 @@ class PriceApi():
             result = Gw0PriceForecast(
                 from_g_node_alias = from_alias.replace("-", "."),
                 hour_start_s = [int(x) for x in unix_times],
-                lmp_list = lmp_prices[:48],
+                lmp_list = lmp_prices,
                 dist_list = dist_prices,
                 energy_list = energy_prices
             )
